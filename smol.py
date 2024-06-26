3af123abd6105850e75ce549a320b21f95a3db5b
Draunzler <54343288+draunzler@users.noreply.github.com>
2024-06-26 23:53:22 +0530

    implemented .env variables

--- smol.py
+++ smol.py
@@ -2,6 +2,10 @@ import discord
 from discord.ext import commands
 import aiohttp
 import random
+import os
+from dotenv import load_dotenv
+load_dotenv()
+TOKEN = os.getenv('ID')
 
 client = discord.Client()
 client = commands.Bot(command_prefix = '!')
@@ -16,4 +20,4 @@ async def meme(ctx):
             embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
             await ctx.send(embed=embed)
 
-client.run('OTYzNDM0NTUyNDk0MjcyNjAy.G_ZR2l.DfjU0eE6tQpqVJNMM42ZhY6KOvsanTQyKH-0w4')
+client.run(TOKEN)
